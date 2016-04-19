9c4a9d1b5a42a959c827c082ed1f3ede392e4a0e
Essa <essa@EssaMacbook.local>
2016-04-19 18:28:15 +0400

    Update the maps

--- eMap/ViewController.swift
+++ eMap/ViewController.swift
@@ -7,12 +7,23 @@
 //
 
 import UIKit
+import ArcGIS
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: AGSMapView!
+    
     override func viewDidLoad() {
+       
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = NSURL(string: "http://services.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer")
+        let mapLayer = AGSTiledMapServiceLayer(URL: url)
+        //self.mapView.minScale = 500000000.0;
+        self.mapView.addMapLayer(mapLayer,withName: "map")
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func changeMap(sender: UISegmentedControl) {
+        
+        
+        var mapURL : NSURL?
+        
+        switch sender.selectedSegmentIndex {
+        case 0:
+            mapURL = NSURL(string: "http://services.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer")
+        default:
+            mapURL = NSURL(string: "http://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer")
+        }
+        
+        self.mapView.removeMapLayerWithName("map")
+        
+        let mapLayer = AGSTiledMapServiceLayer(URL: mapURL)
+        
+        
+        self.mapView.insertMapLayer(mapLayer, withName: "map", atIndex: 0)
+        
+        
+        
+    }
 
 }
 
